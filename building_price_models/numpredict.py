6c08c5655c403f9f452a4920f93cda9caf1f06ce
Kaibo Hao <haokaibo@outlook.com>
2017-12-07 21:06:00 +0800

    implement the knnestimate.

--- building_price_models/numpredict.py
+++ building_price_models/numpredict.py
@@ -35,4 +35,33 @@ def wineset1():
         # Add to the dataset
         rows.append({'input': (rating, age), 'result': price})
 
-    return  rows
+    return rows
+
+
+def euclidean(v1, v2):
+    d = 0.0
+    for i in range(len(v1)):
+        d += (v1[i] - v2[i]) ** 2
+    return math.sqrt(d)
+
+
+def getdistances(data, vec1):
+    distancelist = []
+    for i in range(len(data)):
+        vec2 = data[i]['input']
+        distancelist.append((euclidean(vec1, vec2), i))
+    distancelist.sort()
+    return distancelist
+
+
+def knnestimate(data, vec1, k=3):
+    # Get sorted distances
+    dlist = getdistances(data, vec1)
+    avg = 0.0
+    
+    # Take the average of the top k results
+    for i in range(k):
+        idx = dlist[i][1]
+        avg += data[idx]['result']
+    avg = avg / k
+    return avg
--- tst/building_price_models_tests.py
+++ tst/building_price_models_tests.py
@@ -5,8 +5,8 @@ import os
 
 from building_price_models import numpredict
 
-class BuildingPriceModelTest(unittest.TestCase):
 
+class BuildingPriceModelTest(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.INFO)
         self.base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_data')
@@ -21,3 +21,20 @@ class BuildingPriceModelTest(unittest.TestCase):
         data = numpredict.wineset1()
         logging.info(data[0])
         logging.info(data[1])
+
+    def testEuclidean(self):
+        data = numpredict.wineset1()
+        data0 = data[0]
+        data1 = data[1]
+        logging.info('data0: %s', data0)
+        logging.info('data1: %s', data1)
+
+        logging.info(numpredict.euclidean(data0['input'], data1['input']))
+
+    def testKnnestimate(self):
+        data = numpredict.wineset1()
+        logging.info(numpredict.knnestimate(data, (95.0, 3.0)))
+        logging.info(numpredict.knnestimate(data, (99.0, 3.0)))
+        logging.info(numpredict.knnestimate(data, (99.0, 5.0)))
+        logging.info(numpredict.wineprice(99.0, 5.0))
+        logging.info(numpredict.knnestimate(data, (95.0, 3.0), k=1))
