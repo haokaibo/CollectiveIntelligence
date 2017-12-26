a32f89df85ec8009004b4f6c62fdb1c4780c7ba3
Kaibo Hao <haokaibo@outlook.com>
2017-12-26 19:40:54 +0800

    implement the optimization first execise for group travel case on schedule optimization.

---
+++ tst/optimization_tests.py
@@ -0,0 +1,27 @@
+import unittest
+
+import os
+
+import optimization
+import logging
+
+from optimization.optimization import *
+
+class OptimizationTest(unittest.TestCase):
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        self.base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_data')
+        if not os.path.exists(self.base_dir):
+            os.makedirs(self.base_dir)
+
+        load_data_from_file(os.path.join(self.base_dir, 'optimization', 'schedule.txt'))
+
+    def tearDown(self):
+        pass
+
+    def testPrintfschedule(self):
+        s = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
+        printschedule(s)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
